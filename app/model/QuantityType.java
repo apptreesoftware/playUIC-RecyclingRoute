7b0b33254d655906ae7b753774a04e083dcbaa19
Sam Orozco <sam.orozco@apptreesoftware.com>
2017-01-18 14:20:48 -0800

    Make QuantityType a DataSource. https://github.com/apptreesoftware/revolution/issues/338 UIC Refuse: Unable to add new quantity type #338

--- app/model/QuantityType.java
+++ app/model/QuantityType.java
@@ -1,11 +1,15 @@
 package model;
 
 import com.avaje.ebean.Model;
+import sdk.data.DataSetItem;
+import sdk.data.ServiceConfigurationAttribute;
 import sdk.list.ListItem;
 import sdk.list.ListServiceConfigurationAttribute;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -16,7 +20,7 @@ import java.util.Set;
 @Table(name = "grr_pickup_quantity_type")
 public class QuantityType extends Model {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "quantity_type_id_seq")
     private int id;
 
     @Column(name = "quantity_name")
@@ -41,6 +45,34 @@ public class QuantityType extends Model {
         attributes.add(new ListServiceConfigurationAttribute.Builder(NAME).name("Name").build());
         return attributes;
     }
+
+    public static List<ServiceConfigurationAttribute> getServiceAttributes() {
+        ArrayList<ServiceConfigurationAttribute> attributes = new ArrayList<>();
+        attributes.add(new ServiceConfigurationAttribute.Builder(NAME).name("NAME").canCreateAndRequired().canUpdateAndRequired().build());
+        return attributes;
+    }
+
+
+    public void copyFrom(DataSetItem dataSetItem){
+        this.name = dataSetItem.getStringAttributeAtIndex(NAME);
+        switch( dataSetItem.getCRUDStatus() ){
+            case Create:
+                this.insert();
+                break;
+            case Update:
+                this.id = Integer.parseInt( dataSetItem.getPrimaryKey() );
+                this.update();
+                break;
+        }
+    }
+
+    public DataSetItem copyTo(DataSetItem dataSetItem){
+            dataSetItem.setPrimaryKey(this.id + "");
+            dataSetItem.setStringForAttributeIndex(this.name, NAME);
+            return dataSetItem;
+    }
+
+
 
     private static int NAME = 0;
 
