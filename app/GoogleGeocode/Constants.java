574411255773da46db651738c9881da5acde2a46
Sam Orozco <sam.orozco@apptreesoftware.com>
2017-01-19 17:14:56 -0800

    Create functionality to Get Lat and Long from Address as well as sent route stops back to client to stop records from being duplicated. https://github.com/apptreesoftware/playUIC-RecyclingRoute/issues/15 Edit route stops creates duplicate records in route #15

---
+++ app/GoogleGeocode/Constants.java
@@ -0,0 +1,13 @@
+package GoogleGeocode;
+
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+/**
+ * Created by eddie on 11/21/16.
+ */
+public class Constants {
+    public static DateTimeFormatter DB_DATE_FORMATTER = DateTimeFormat.forPattern("yyyy-MM-dd").withZone(DateTimeZone.forID("Etc/GMT"));
+    public static DateTimeFormatter DB_DATETIME_FORMATTER = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").withZone(DateTimeZone.forID("Etc/GMT"));;
+}
