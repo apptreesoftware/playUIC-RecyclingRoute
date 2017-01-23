979e6159311b4f1562f5586c104d263da2a6c047
Sam Orozco <sam.orozco@apptreesoftware.com>
2017-01-23 09:52:58 -0800

    Changed the time zone in constants file. It is not being used anywhere.

--- app/util/Constants.java
+++ app/util/Constants.java
@@ -8,6 +8,6 @@ import org.joda.time.format.DateTimeFormatter;
  * Created by eddie on 11/21/16.
  */
 public class Constants {
-    public static DateTimeFormatter DB_DATE_FORMATTER = DateTimeFormat.forPattern("yyyy-MM-dd").withZone(DateTimeZone.forID("Etc/GMT"));
-    public static DateTimeFormatter DB_DATETIME_FORMATTER = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").withZone(DateTimeZone.forID("Etc/GMT"));;
+    public static DateTimeFormatter DB_DATE_FORMATTER = DateTimeFormat.forPattern("yyyy-MM-dd").withZone(DateTimeZone.UTC);
+    public static DateTimeFormatter DB_DATETIME_FORMATTER = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").withZone(DateTimeZone.UTC);;
 }
